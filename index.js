e48e53a8ae9c14aa00a84da3fde639ec41f2dfb2
Milton <miltonsoftwareengineer@gmail.com>
2022-08-20 17:46:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(feet <= 400) {
+    return 'This one is on me!';
+  }else if(feet > 2000 &&  feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else if(feet > 2500) {
+    return 'No can do.';
+  }
+  
+}
+function ternaryCheckCity(city){
+    if (city==="NYC"){
+      return ("Ok, sounds good.")
+    }else if (city==="Pittsburgh"){
+      return ("No go.")
+}
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    case 'thanks for everything':
+    return 'Bye.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+//   describe('switchOnCharmFromTip()', function () {
+//     it('should return "Thank you so much." if the tip is generous', function () {
+//       expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+//     });
+
+//     it('should return "Thank you." if the tip is not as generous', function () {
+//       expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+//     });
+
+//     it('should return "Bye." if anything else', function () {
+//       expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+//     });
+//   });
+// });
